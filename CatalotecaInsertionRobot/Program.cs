cc8103b512f5765af83b1efc327620ec6333ec5c
Lucas Siqueira <lalvessiqueira8@gmail.com>
2020-01-13 23:02:27 -0300

    first commit

---
+++ CatalotecaInsertionRobot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CatalotecaInsertionRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# cataloteca-insertion-robot" 
