c1003a936b6a90e252568a8d14e5b953e93c8ed3
Lucas Siqueira <lalvessiqueira8@gmail.com>
2020-02-10 00:10:29 -0300

    add mysql insert

--- CatalotecaInsertionRobot/app/Program.cs
+++ CatalotecaInsertionRobot/app/Program.cs
@@ -1,57 +1,117 @@
-﻿using System.Data.SqlClient;
-using System;
-using System.Data;
-using System.IO;
-using System.Linq;
+﻿using System;
 using System.Diagnostics;
 using CatalotecaInsertionRobot.src.Utils;
+using System.Collections.Generic;
+using CatalotecaInsertionRobot.app.src;
+using System.Linq;
 using CatalotecaInsertionRobot.src.Data;
 
 namespace CatalotecaInsertionRobot
 {
-  class Program
-  {
-    private const string tablename = "rawdata";
-
-    public static void Main()
+    class Program
     {
-      Console.WriteLine("Robô de inserção Cataloteca");
-      Console.WriteLine("-------------------------");
+        private const string tablename = "product";
 
-      // Input de dados
-      Console.Write("Digite qual o gerenciador de banco de dados (1-MySQL, 2-SQLServer):");
-      string sgbd = Console.ReadLine();
+        public static void Main()
+        {
+            Console.WriteLine("Robô de inserção Cataloteca");
+            Console.WriteLine("-------------------------");
 
-      Console.Write("Digite host do banco de dados (Default => localhost):");
-      string server = Console.ReadLine();
+            // Input de dados
+            Console.Write("Digite qual o gerenciador de banco de dados (1-MySQL, 2-SQLServer):");
+            string sgbd = Console.ReadLine();
 
-      Console.Write("Digite o nome do banco de dados (Default => cataloteca):");
-      string dbName = Console.ReadLine();
+            Console.Write("Digite host do banco de dados (Default => localhost):");
+            string server = Console.ReadLine();
 
-      Console.WriteLine("Digite o caminho completo para o arquivo:");
-      string filePath = @Console.ReadLine();
+            Console.Write("Digite usuario (Default => root):");
+            string user = Console.ReadLine();
 
-      string stringConnection = Utils.GetStringConnection(sgbd, server, dbName);
+            Console.Write("Digite a senha: ");
+            string password = CustomConsole.ReadPassword();
 
-      // Iniciando count de tempo de processamento
-      var stopwatch = new Stopwatch();
-      stopwatch.Start();
+            Console.Write("Digite o nome do banco de dados (Default => cataloteca):");
+            string dbName = Console.ReadLine();
 
-      Console.WriteLine("-------------------------");
-      Console.WriteLine("Iniciando leitura da planilha...");
-      var dt = Utils.GetDataTableFromExcel(filePath);
+            Console.WriteLine("Digite o caminho completo para o arquivo:");
+            string filePath = @Console.ReadLine();
 
-      Console.WriteLine("Iniciando Inserção...");
-      Data.InsertToSQLUsingSQLBulk(dt, stringConnection, tablename);
+            string stringConnection = Utils.GetStringConnection(sgbd, server, dbName, user, password);
+
+            // Iniciando count de tempo de processamento
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Iniciando leitura da planilha...");
+            List<ProductEntity> dt = Utils.GetDataTableFromExcel(filePath);
+
+            Console.WriteLine("Iniciando Inserção...");
+            ForwardsInsertion(stringConnection, sgbd, dt, tablename);
 
 
-      Console.WriteLine("-------------------------");
-      // Terminando count de tempo de processamento
-      Console.WriteLine($"Tempo de processamento (Em segundos): {stopwatch.Elapsed.TotalSeconds}");
-      stopwatch.Stop();
-      Console.WriteLine("Fim!");
+            Console.WriteLine("-------------------------");
+            // Terminando count de tempo de processamento
+            Console.WriteLine($"Tempo de processamento (Em segundos): {stopwatch.Elapsed.TotalSeconds}");
+            stopwatch.Stop();
+            Console.WriteLine("Fim!");
 
+        }
+        private static void ForwardsInsertion(string stringConnection, string sgbd, List<ProductEntity> content, string table)
+        {
+            if (sgbd == "1")
+            {
+                MysqlData.InsertMySQL(stringConnection, content, table);
+            } else
+            {
+                SqlServerData.InsertSQLServer(stringConnection, content, table);
+            }
+        }
+
+        public class CustomConsole
+        {
+            public static string ReadPassword(char mask)
+            {
+                const int ENTER = 13, BACKSP = 8, CTRLBACKSP = 127;
+                int[] FILTERED = { 0, 27, 9, 10 /*, 32 space, if you care */ }; // const
+
+                var pass = new Stack<char>();
+                char chr = (char)0;
+
+                while ((chr = System.Console.ReadKey(true).KeyChar) != ENTER)
+                {
+                    if (chr == BACKSP)
+                    {
+                        if (pass.Count > 0)
+                        {
+                            System.Console.Write("\b \b");
+                            pass.Pop();
+                        }
+                    }
+                    else if (chr == CTRLBACKSP)
+                    {
+                        while (pass.Count > 0)
+                        {
+                            System.Console.Write("\b \b");
+                            pass.Pop();
+                        }
+                    }
+                    else if (FILTERED.Count(x => chr == x) > 0) { }
+                    else
+                    {
+                        pass.Push((char)chr);
+                        System.Console.Write(mask);
+                    }
+                }
+
+                System.Console.WriteLine();
+
+                return new string(pass.Reverse().ToArray());
+            }
+            public static string ReadPassword()
+            {
+                return ReadPassword('*');
+            }
+        }
     }
-
-  }
 }
--- CatalotecaInsertionRobot/app/src/Utils.cs
+++ CatalotecaInsertionRobot/app/src/Utils.cs
@@ -1,77 +1,87 @@
-using System.Data.SqlClient;
 using System;
 using System.Data;
 using System.IO;
 using System.Linq;
-using System.Diagnostics;
+using System.Collections.Generic;
+using CatalotecaInsertionRobot.app.src;
 
 namespace CatalotecaInsertionRobot.src.Utils
 {
-  public class Utils
-  {
-    public static string GetStringConnection(string sgbd, string server, string dbName)
+    public class Utils
     {
-      server = server == "" ? "localhost" : server;
-      dbName = dbName == "" ? "cataloteca" : dbName;
-      if (sgbd == "1") //Mysql
-      {
-        string stringConnection = $"Host={server};Database={dbName};";
-      }
-      else
-      { // SQLSERVER
-        string stringConnection = $"Server={server}; Initial Catalog = {dbName}";
-      }
-      return stringConnection;
-    }
-
-    public static DataTable GetDataTableFromExcel(string path, bool hasHeader = true)
-    {
-      using (var pck = new OfficeOpenXml.ExcelPackage())
-      {
-        // Abre arquivo
-        using (var stream = File.OpenRead(path))
+        public static string GetStringConnection(string sgbd, string server, string dbName, string user, string pass)
         {
-          pck.Load(stream);
-        }
-
-        // Pega primeira planilha
-        var ws = pck.Workbook.Worksheets.First();
-
-        // Define objeto de database
-        DataTable database = new DataTable();
-
-
-        foreach (var firstRowCell in ws.Cells[1, 1, 1, ws.Dimension.End.Column])
-        {
-          database.Columns.Add(hasHeader ? firstRowCell.Text : string.Format("Column {0}", firstRowCell.Start.Column));
-        }
-
-        // Se tiver Header a primeira linha será a 2
-        var startRow = hasHeader ? 2 : 1;
-
-        // For em cada linha da planilha a partir da primeira
-        for (int rowNum = startRow; rowNum <= ws.Dimension.End.Row; rowNum++)
-        {
-          try
-          {
-            var wsRow = ws.Cells[rowNum, 1, rowNum, ws.Dimension.End.Column];
-            DataRow row = database.Rows.Add();
-            foreach (var cell in wsRow)
+            string stringConnection;
+            user = user == "" ? "root" : user;
+            server = server == "" ? "localhost" : server;
+            dbName = dbName == "" ? "cataloteca" : dbName;
+            if (sgbd == "1") //Mysql
             {
-              row[cell.Start.Column - 1] = cell.Text;
+                stringConnection = $"Server={server};Database={dbName};Uid={user};Pwd={pass};";
             }
-          }
-          catch (Exception ex)
-          {
-            Console.WriteLine($"Erro na linha {rowNum}");
-            Console.WriteLine(ex);
-
-          }
-
+            else
+            { // SQLSERVER
+                stringConnection = $"Server={server};Database={dbName};Trusted_Connection=True;";
+            }
+            return stringConnection;
         }
-        return database;
-      }
-    }
 
-  }
+        public static List<ProductEntity> GetDataTableFromExcel(string path, bool hasHeader = true)
+        {
+            using (var pck = new OfficeOpenXml.ExcelPackage())
+            {
+                // Abre arquivo
+                using (var stream = File.OpenRead(path))
+                {
+                    pck.Load(stream);
+                }
+
+                // Pega primeira planilha
+                var ws = pck.Workbook.Worksheets.First();
+
+                // Define objeto de database
+                DataTable database = new DataTable();
+
+
+                foreach (var firstRowCell in ws.Cells[1, 1, 1, ws.Dimension.End.Column])
+                {
+                    database.Columns.Add(hasHeader ? firstRowCell.Text : string.Format("Column {0}", firstRowCell.Start.Column));
+                }
+
+                // Se tiver Header a primeira linha será a 2
+                var startRow = hasHeader ? 2 : 1;
+
+                // For em cada linha da planilha a partir da primeira
+                for (int rowNum = startRow; rowNum <= ws.Dimension.End.Row; rowNum++)
+                {
+                    try
+                    {
+                        var wsRow = ws.Cells[rowNum, 1, rowNum, ws.Dimension.End.Column];
+                        DataRow row = database.Rows.Add();
+                        foreach (var cell in wsRow)
+                        {
+                            row[cell.Start.Column - 1] = cell.Text;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Erro na linha {rowNum}");
+                        Console.WriteLine(ex);
+
+                    }
+
+                }
+                List<ProductEntity> productList = new List<ProductEntity>();
+                productList = (from DataRow dr in database.Rows
+                               select new ProductEntity()
+                               {
+                                   Name = dr["Name"].ToString(),
+                                   ShortDescription = dr["Name"].ToString(),
+                                   LongDescription = dr["Description"].ToString()
+                               }).ToList();
+                return productList;
+            }
+        }
+
+    }
 }
